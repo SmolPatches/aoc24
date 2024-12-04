c2fafd70dd12632729f2e25b95bdfe1a2947894d
mdnlss <rob73hall@gmail.com>
2024-12-04 01:16:19 -0500

    done: day3p1

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,9 +1,47 @@
-use log;
 use env_logger;
-use regex;
-fn part1() {
-    //regex: (mul\(\d+,\d+\))
+use log::{self, info};
+use regex::Regex;
+type Num = u32;
+struct Op {
+    x: Num,
+    y: Num,
+}
+impl Op {
+    pub fn new(x: Num, y: Num) -> Op {
+        Op { x, y }
+    }
+    pub fn calc(&self) -> Num {
+        self.x * self.y
+    }
+}
+fn part1(input: &str) {
+    // capture the digits in a regex
+    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    let ops: Vec<Op> = re
+        .captures_iter(input)
+        .filter_map(|reg_match| {
+            // note this will silently fail if parsing fails
+            let (x, y) = (
+                reg_match.get(1)?.as_str().parse::<Num>().ok()?,
+                reg_match.get(2)?.as_str().parse::<Num>().ok()?,
+            );
+            info!(target:"Operand Filter","{:?}x{:?}",x,y);
+            Some((x, y))
+        })
+        .map(|v| Op::new(v.0, v.1))
+        .collect();
+    println!(
+        "Total: {}",
+        ops.iter().fold(0, |total, op| total + op.calc())
+    );
 }
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+    part1(include_str!("../files/input"));
+}
+
+#[test]
+fn test_part1() {
+    env_logger::init();
+    part1(include_str!("../files/test"))
 }
